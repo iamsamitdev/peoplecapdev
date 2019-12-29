9e0499d73890d5e49412e5063c53f2a8d52a2f19
Samit Koyom <samitkoyom@gmail.com>
2019-12-30 00:37:46 +0700

    deploy

---
+++ js/chunk-2d0d7ab6.015334c6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d7ab6"],{7898:function(n,t,e){"use strict";e.r(t);var a=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",{staticClass:"blank_layout"},[e("router-view")],1)},l=[],u=e("2877"),s={},c=Object(u["a"])(s,a,l,!1,null,null,null);t["default"]=c.exports}}]);
+//# sourceMappingURL=chunk-2d0d7ab6.015334c6.js.map
